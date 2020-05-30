3d2ef63bfee9573691dc4508bf2036c096951c85
Saurav Santara <joyz7298@gmail.com>
2020-05-30 17:26:39 +0530

    Comment

--- shoppingbackend/src/test/java/com/niit/shoppingbackend/test/CartLineTestCase.java
+++ shoppingbackend/src/test/java/com/niit/shoppingbackend/test/CartLineTestCase.java
@@ -1,73 +1,73 @@
-package com.niit.shoppingbackend.test;
-
-import static org.junit.Assert.assertEquals;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import com.niit.shoppingbackend.dao.CartLineDAO;
-import com.niit.shoppingbackend.dao.ProductDAO;
-import com.niit.shoppingbackend.dao.UserDAO;
-import com.niit.shoppingbackend.dto.Cart;
-import com.niit.shoppingbackend.dto.CartLine;
-import com.niit.shoppingbackend.dto.Product;
-import com.niit.shoppingbackend.dto.User;
-
-public class CartLineTestCase {
-
-	private static AnnotationConfigApplicationContext context;
-	private static CartLineDAO cartLineDAO = null;
-	private static ProductDAO productDAO = null;
-	private static UserDAO userDAO = null;
-	
-	private Product product = null;
-	private User user = null;
-	private Cart cart = null;
-	private CartLine cartLine = null;
-	
-	@BeforeClass
-	public static void init() {
-		context = new AnnotationConfigApplicationContext();
-		context.scan("com.niit.shoppingbackend");
-		context.refresh();
-		productDAO = (ProductDAO)context.getBean("productDAO");
-		userDAO = (UserDAO)context.getBean("userDAO");
-		cartLineDAO = (CartLineDAO)context.getBean("cartLineDAO");			
-	}	
-		
-	@Test
-	public void testAddNewCartLine() {
-		
-		// 1. get the user
-		user = userDAO.getByEmail("mm@gmail.com");
-		
-		// 2. fetch the cart
-		cart = user.getCart();
-		
-		// 3. get the product
-		product = productDAO.get(1);
-		
-		// 4. create a new cartline
-		cartLine = new CartLine();
-		
-		cartLine.setBuyingPrice(product.getUnitPrice());
-		
-		cartLine.setProductCount(cartLine.getProductCount() + 1);
-		
-		cartLine.setTotal(cartLine.getProductCount() * product.getUnitPrice());
-		
-		cartLine.setAvailable(true);
-		
-		cartLine.setCartId(cart.getId());
-
-		cartLine.setProduct(product);
-		
-		assertEquals("Failed to add the cartLine", true, cartLineDAO.add(cartLine));
-		
-		// update the cart
-		cart.setGrandTotal(cart.getGrandTotal() + cartLine.getTotal());
-		cart.setCartLines(cart.getCartLines() + 1);
-		assertEquals("Failed to update the cart", true, cartLineDAO.updateCart(cart));
-	}
-}
+//package com.niit.shoppingbackend.test;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//
+//import com.niit.shoppingbackend.dao.CartLineDAO;
+//import com.niit.shoppingbackend.dao.ProductDAO;
+//import com.niit.shoppingbackend.dao.UserDAO;
+//import com.niit.shoppingbackend.dto.Cart;
+//import com.niit.shoppingbackend.dto.CartLine;
+//import com.niit.shoppingbackend.dto.Product;
+//import com.niit.shoppingbackend.dto.User;
+//
+//public class CartLineTestCase {
+//
+//	private static AnnotationConfigApplicationContext context;
+//	private static CartLineDAO cartLineDAO = null;
+//	private static ProductDAO productDAO = null;
+//	private static UserDAO userDAO = null;
+//	
+//	private Product product = null;
+//	private User user = null;
+//	private Cart cart = null;
+//	private CartLine cartLine = null;
+//	
+//	@BeforeClass
+//	public static void init() {
+//		context = new AnnotationConfigApplicationContext();
+//		context.scan("com.niit.shoppingbackend");
+//		context.refresh();
+//		productDAO = (ProductDAO)context.getBean("productDAO");
+//		userDAO = (UserDAO)context.getBean("userDAO");
+//		cartLineDAO = (CartLineDAO)context.getBean("cartLineDAO");			
+//	}	
+//		
+//	@Test
+//	public void testAddNewCartLine() {
+//		
+//		// 1. get the user
+//		user = userDAO.getByEmail("mm@gmail.com");
+//		
+//		// 2. fetch the cart
+//		cart = user.getCart();
+//		
+//		// 3. get the product
+//		product = productDAO.get(1);
+//		
+//		// 4. create a new cartline
+//		cartLine = new CartLine();
+//		
+//		cartLine.setBuyingPrice(product.getUnitPrice());
+//		
+//		cartLine.setProductCount(cartLine.getProductCount() + 1);
+//		
+//		cartLine.setTotal(cartLine.getProductCount() * product.getUnitPrice());
+//		
+//		cartLine.setAvailable(true);
+//		
+//		cartLine.setCartId(cart.getId());
+//
+//		cartLine.setProduct(product);
+//		
+//		assertEquals("Failed to add the cartLine", true, cartLineDAO.add(cartLine));
+//		
+//		// update the cart
+//		cart.setGrandTotal(cart.getGrandTotal() + cartLine.getTotal());
+//		cart.setCartLines(cart.getCartLines() + 1);
+//		assertEquals("Failed to update the cart", true, cartLineDAO.updateCart(cart));
+//	}
+//}
--- shoppingbackend/src/test/java/com/niit/shoppingbackend/test/CategoryTestCase.java
+++ shoppingbackend/src/test/java/com/niit/shoppingbackend/test/CategoryTestCase.java
@@ -1,109 +1,109 @@
-package com.niit.shoppingbackend.test;
-
-import static org.junit.Assert.assertEquals;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import com.niit.shoppingbackend.dao.CategoryDAO;
-import com.niit.shoppingbackend.dto.Category;
-
-public class CategoryTestCase {
-
-	private static AnnotationConfigApplicationContext context;
-	
-	private static CategoryDAO categoryDAO;
-	
-	private Category category;
-	
-	@BeforeClass
-	public static void init() {
-		
-		context = new AnnotationConfigApplicationContext();
-		context.scan("com.niit.shoppingbackend");
-		context.refresh();
-		
-		categoryDAO = (CategoryDAO)context.getBean("categoryDAO");
-	}
-	
-	
-	/*
-	 * @Test public void testAddCategory() {
-	 * 
-	 * category = new Category();
-	 * 
-	 * category.setName("Television");
-	 * category.setDescription("Some description about television.");
-	 * category.setImageURL("CAT_4.png");
-	 * 
-	 * assertEquals("Successfully added a category inside the table!", true,
-	 * categoryDAO.add(category)); }
-	 */
-	
-	/*
-	 * @Test public void testGetCategory() {
-	 * 
-	 * category = categoryDAO.get(1);
-	 * 
-	 * assertEquals("Successfully fetched a single category from the table!",
-	 * "Television", category.getName()); }
-	 */
-	
-	/*
-	 * @Test public void testUpdateCategory() {
-	 * 
-	 * category = categoryDAO.get(1); category.setName("TV");
-	 * assertEquals("Successfully updated a single category in the table!", true,
-	 * categoryDAO.update(category)); }
-	 */
-	
-	
-	/*
-	 * @Test public void testDeleteCategory() {
-	 * 
-	 * category = categoryDAO.get(1);
-	 * assertEquals("Successfully deleted a single category in the table!", true,
-	 * categoryDAO.delete(category)); }
-	 */
-	
-	/*
-	 * @Test public void testListCategory() {
-	 * 
-	 * assertEquals("Successfully fetched a single category in the table!", 3,
-	 * categoryDAO.list().size()); }
-	 */
-	
-	@Test
-	public void testCRUDCategory() {
-		
-		// add operation
-		category = new Category();
-		  
-		category.setName("Laptop");
-		category.setDescription("Some description about laptop.");
-		category.setImageURL("CAT_1.png");
-		  
-		assertEquals("Successfully added a category inside the table!", true, categoryDAO.add(category));
-		
-		category = new Category();
-		  
-		category.setName("Television");
-		category.setDescription("Some description about television.");
-		category.setImageURL("CAT_2.png");
-		  
-		assertEquals("Successfully added a category inside the table!", true, categoryDAO.add(category));
-		
-		// fetch and update operations
-		category = categoryDAO.get(2);
-		category.setName("TV");
-		assertEquals("Successfully updated a single category in the table!", true, categoryDAO.update(category));
-		
-		// delete operation
-		assertEquals("Successfully deleted a single category in the table!", true, categoryDAO.delete(category));
-		
-		// fetching the list
-		assertEquals("Successfully fetched a single category in the table!", 1, categoryDAO.list().size());
-	}
-		 
-}
+//package com.niit.shoppingbackend.test;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//
+//import com.niit.shoppingbackend.dao.CategoryDAO;
+//import com.niit.shoppingbackend.dto.Category;
+//
+//public class CategoryTestCase {
+//
+//	private static AnnotationConfigApplicationContext context;
+//	
+//	private static CategoryDAO categoryDAO;
+//	
+//	private Category category;
+//	
+//	@BeforeClass
+//	public static void init() {
+//		
+//		context = new AnnotationConfigApplicationContext();
+//		context.scan("com.niit.shoppingbackend");
+//		context.refresh();
+//		
+//		categoryDAO = (CategoryDAO)context.getBean("categoryDAO");
+//	}
+//	
+//	
+//	/*
+//	 * @Test public void testAddCategory() {
+//	 * 
+//	 * category = new Category();
+//	 * 
+//	 * category.setName("Television");
+//	 * category.setDescription("Some description about television.");
+//	 * category.setImageURL("CAT_4.png");
+//	 * 
+//	 * assertEquals("Successfully added a category inside the table!", true,
+//	 * categoryDAO.add(category)); }
+//	 */
+//	
+//	/*
+//	 * @Test public void testGetCategory() {
+//	 * 
+//	 * category = categoryDAO.get(1);
+//	 * 
+//	 * assertEquals("Successfully fetched a single category from the table!",
+//	 * "Television", category.getName()); }
+//	 */
+//	
+//	/*
+//	 * @Test public void testUpdateCategory() {
+//	 * 
+//	 * category = categoryDAO.get(1); category.setName("TV");
+//	 * assertEquals("Successfully updated a single category in the table!", true,
+//	 * categoryDAO.update(category)); }
+//	 */
+//	
+//	
+//	/*
+//	 * @Test public void testDeleteCategory() {
+//	 * 
+//	 * category = categoryDAO.get(1);
+//	 * assertEquals("Successfully deleted a single category in the table!", true,
+//	 * categoryDAO.delete(category)); }
+//	 */
+//	
+//	/*
+//	 * @Test public void testListCategory() {
+//	 * 
+//	 * assertEquals("Successfully fetched a single category in the table!", 3,
+//	 * categoryDAO.list().size()); }
+//	 */
+//	
+//	@Test
+//	public void testCRUDCategory() {
+//		
+//		// add operation
+//		category = new Category();
+//		  
+//		category.setName("Laptop");
+//		category.setDescription("Some description about laptop.");
+//		category.setImageURL("CAT_1.png");
+//		  
+//		assertEquals("Successfully added a category inside the table!", true, categoryDAO.add(category));
+//		
+//		category = new Category();
+//		  
+//		category.setName("Television");
+//		category.setDescription("Some description about television.");
+//		category.setImageURL("CAT_2.png");
+//		  
+//		assertEquals("Successfully added a category inside the table!", true, categoryDAO.add(category));
+//		
+//		// fetch and update operations
+//		category = categoryDAO.get(2);
+//		category.setName("TV");
+//		assertEquals("Successfully updated a single category in the table!", true, categoryDAO.update(category));
+//		
+//		// delete operation
+//		assertEquals("Successfully deleted a single category in the table!", true, categoryDAO.delete(category));
+//		
+//		// fetching the list
+//		assertEquals("Successfully fetched a single category in the table!", 1, categoryDAO.list().size());
+//	}
+//		 
+//}
